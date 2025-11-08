4e6b30fd11efc348d6d94709fa68d6a79260e306
sahilvaghela105 <147694974+sahilvaghela105@users.noreply.github.com>
2025-11-08 09:19:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,16 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
+  base: "/MyProtfolio/", 
+
   server: {
     host: "::",
-    port: 8080,
+    port: 8088,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/' // Replace with your GitHub repo name
 }));
